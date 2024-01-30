4ecac5579bde228465588cf31101a2a4f4ef0033
Mehrdad <74200693+mehrdadalmasi2020@users.noreply.github.com>
2024-01-30 16:24:52 +0100

    Create cleansing.py

---
+++ cleansing.py
@@ -0,0 +1,18 @@
+path='/mnt/data/c2dh-dri/'
+read=open(path+"/try/"+"test.txt","r",encoding='utf-8')
+wholeText=""
+for e in read:
+    wholeText=wholeText+e
+
+QApart=wholeText.split("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+Flag=False
+CleanText=""
+for e in QApart[1].split("\n"):
+    if "?" in e or Flag==True:
+        CleanText=CleanText+e
+        Flag=False
+        CleanText=CleanText+e
+        print(e)
+    if "?" in e :
+        Flag=True
