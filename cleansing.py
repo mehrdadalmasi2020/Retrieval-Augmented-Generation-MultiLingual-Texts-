89221a008b3e2c62d433f282c3eae45e8d9313c8
Mehrdad <74200693+mehrdadalmasi2020@users.noreply.github.com>
2024-02-07 19:35:45 +0100

    Update cleansing.py

--- cleansing.py
+++ cleansing.py
@@ -1,19 +1,19 @@
-path='/mnt/data/'
-read=open(path+"/try/"+"test.txt","r",encoding='utf-8')
-wholeText=""
-for e in read:
-    wholeText=wholeText+e
-
-QApart=wholeText.split("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-
-Flag=False
-CleanText=""
-for e in QApart[1].split("\n"):
-    if "?" in e or Flag==True:
-        CleanText=CleanText+e
-        Flag=False
-        CleanText=CleanText+e
-        print(e)
-    if "?" in e :
-        Flag=True
-return CleanText
+def Clean(path):
+    read=open(path,"r",encoding='utf-8')
+    wholeText=""
+    for e in read:
+        wholeText=wholeText+e
+    
+    QApart=wholeText.split("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    
+    Flag=False
+    CleanText=""
+    for e in QApart[1].split("\n"):
+        if "?" in e or Flag==True:
+            CleanText=CleanText+e
+            Flag=False
+            CleanText=CleanText+e
+            print(e)
+        if "?" in e :
+            Flag=True
+    return CleanText
